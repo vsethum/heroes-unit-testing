7bcde982ae02d9af1f55ee6d25b0645738f662ed
Vijay Sethumadavan <sethumadavan@gmail.com>
2017-09-18 12:03:01 -0400

    First commit

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,25 @@
+import { HeroService } from './shared/hero.service';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+
+import { HeroesComponent } from './heroes.component';
+
+describe('HeroesComponent', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent],
+      providers: [HeroService],
+      imports: [HttpModule, RouterTestingModule]
+    });
+    TestBed.compileComponents();
+  }));
+
+  it('should create the hero app', async(() => {
+    const fixture = TestBed.createComponent(HeroesComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+ 
+});
