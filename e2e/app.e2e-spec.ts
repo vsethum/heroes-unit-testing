2432bba877b1cce8cc3201ceb4237da49e911b59
Vijay Sethumadavan <sethumadavan@gmail.com>
2017-09-20 14:35:21 -0400

    First commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { AngularTourOfHeroesPage } from './app.po';
+import { HeroesListPage } from './heroes-list-page';
 
 describe('angular-tour-of-heroes App', () => {
-  let page: AngularTourOfHeroesPage;
+  let angularTourOfHeroesPage: AngularTourOfHeroesPage;
+  let heroesListPage: HeroesListPage;
 
   beforeEach(() => {
-    page = new AngularTourOfHeroesPage();
+    angularTourOfHeroesPage = new AngularTourOfHeroesPage();
+    heroesListPage = new HeroesListPage();
   });
 
   it('should display message saying Tour of Heroes', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Tour of Heroes');
+    angularTourOfHeroesPage.navigateTo();
+    expect(angularTourOfHeroesPage.getParagraphText()).toEqual('Tour of Heroes');
   });
-});
+
+  it('should display message saying My Heroes', () => {
+    heroesListPage.navigateTo();
+    expect(heroesListPage.getParagraphText()).toEqual('My Heroes');
+  });
+
+  it('should contain more than 5 heroes', () => {
+    heroesListPage.navigateTo();
+    expect(`heroesListPage.getNumberOfHeroes().toBeGreaterThan(5)`);
+    }
+  );
+
+ });
